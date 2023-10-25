5fab166b6cbba4040ce2029e6560ac8d88bfc894
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-10-25 16:52:34 +0200

    feat: Armazón hecho, faltan más regexp.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var codigo_act = document.getElementById("codigo");
 var boton_enviar = document.getElementById("enviar");
+var contenerdor = document.getElementById("container");
 
 //USO DE APIs PARA RECOGER INFORMACIÓN EXTERNA
 
@@ -16,20 +17,42 @@ var expresion_regular_1 = /^.{10,}$/;
 var expresion_regular_2 = /^[0-9]/;
 
 //El código debe contener al ganador de los worlds del LOL en 2011 escrito en tres letras mayúsculas
-
 var expresion_regular_3 = /.*FNC.*/;
 
 //---------------------------------------------
 
-//FUNCIÓN PARA FABRICAR UN ERROR
+//FUNCIÓN PARA FABRICAR & LIMPIAR UN ERROR
 
-function crea_errpr(msg){
+function crea_error(msg){
  //crear un div con la clase error
     var div_error = document.createElement("div");
     div_error.classList.add("error");
 
     var p_error = document.createElement("p");
     p_error.classList.add("error_msg");
+    p_error.innerHTML = msg;
+
+    div_error.appendChild(p_error);
+    contenerdor.appendChild(div_error);
+}
+
+function limpia_error(msg){
+    //Cogemos todos los div con clase error que tienen el mensaje de error que queremos borrar
+    var errores = document.querySelectorAll(".error_msg");
+
+    //Recorremos el array de errores
+    for (var i = 0; i < errores.length; i++){
+
+        //Si el mensaje de error es el que queremos borrar
+        if (errores[i].innerHTML == msg){
+
+            //Borramos el div que contiene el mensaje de error
+            errores[i].parentNode.remove();
+
+        }
+
+    }
+    
 }
 
 //FUNCIÓN DE VALIDACIÓN
@@ -40,31 +63,33 @@ function validar_codigo(event){
 
     if(expresion_regular_1.test(codigo_act.value)){
 
-        console.log("Código tiene al menos 10 caracteres");
+        limpia_error("El código debe tener al menos 10 caracteres");
 
         if (expresion_regular_2.test(codigo_act.value)){
 
-            console.log("Código comienza por un dígito");
+            limpia_error("El código debe comenzar por un dígito");
 
             if (expresion_regular_3.test(codigo_act.value)){
 
-                console.log("Código contiene las letras FNC");
+                limpia_error("El código debe contener el equipo de esports ganador de los worlds del LOL en 2011 (formato FGT)");
+
+                alert("Código correcto");
 
             }else{
 
-                console.log("Código no contiene las letras FNC");
+                crea_error("El código debe contener el equipo de esports ganador de los worlds del LOL en 2011 (formato FGT)");
 
             }
 
         }else{
 
-            console.log("Código no comienza por un dígito");
+            crea_error("El código debe comenzar por un dígito");
 
         }
 
     }else{
 
-        console.log("Código no tiene al menos 10 caracteres");
+        crea_error("El código debe tener al menos 10 caracteres");
 
     }
 
